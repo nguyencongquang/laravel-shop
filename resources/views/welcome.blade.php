dea1064919557db82bc666644e829ad084870919
unknown <quang.nguyencong@gmail.com>
2023-02-13 10:55:32 +0700

    Add authenticate function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -58,7 +58,7 @@
   </div>
   <nav class="humberger__menu__nav mobile-menu">
     <ul>
-      <li class="active"><a href="#">Home</a></li>
+      <li class="active"><a href="#">Dashboard</a></li>
       <li><a href="#">Shop</a></li>
       <li><a href="#">Pages</a>
         <ul class="header__menu__dropdown">
@@ -133,7 +133,7 @@
       <div class="col-lg-6">
         <nav class="header__menu">
           <ul>
-            <li class="active"><a href="#">Home</a></li>
+            <li class="active"><a href="#">Dashboard</a></li>
             <li><a href="#">Shop</a></li>
             <li><a href="#">Pages</a>
               <ul class="header__menu__dropdown">
